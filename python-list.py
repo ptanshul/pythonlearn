1ce63867dfd3fac76e7acddff6b8b6dc583e1a73
anshulmb <anshultiwarimb@gmail.com>
2025-07-04 15:05:16 +0530

    commit 3

--- python-list.py
+++ python-list.py
@@ -4,3 +4,5 @@ print("The list is:", thelist)
 
 #List functions
 print("The first element is:", thelist[0])
+print("The last element is:", thelist[-1])
+print("The first two elements are:", thelist[0:2])  
