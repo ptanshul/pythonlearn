182672ddc80e14129bdd21b4f674e43ab1a05dc5
anshulmb <anshultiwarimb@gmail.com>
2025-07-04 15:30:55 +0530

    c4

--- python-list.py
+++ python-list.py
@@ -6,3 +6,10 @@ print("The list is:", thelist)
 print("The first element is:", thelist[0])
 print("The last element is:", thelist[-1])
 print("The first two elements are:", thelist[0:2])  
+
+#List methods
+thelist.append("six")
+print("After appending 'six':", thelist)
+thelist.remove("three") 
+print("After removing 'three':", thelist)
+thelist.insert(2, "two and a half")
